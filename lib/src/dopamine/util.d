31b51ccae0ab333d9e034f00be8ed6d5d2111887
Remi Thebault <remi.thebault@gmail.com>
2020-12-19 07:54:15 +0100

    check flag exist before remove
    + format util

--- lib/src/dopamine/util.d
+++ lib/src/dopamine/util.d
@@ -5,31 +5,30 @@ import std.traits;
 
 @safe:
 
-void feedDigestData(D)(ref D digest, in string s)
-if (isDigest!D)
+void feedDigestData(D)(ref D digest, in string s) if (isDigest!D)
 {
-    digest.put(cast(const(ubyte)[])s);
+    digest.put(cast(const(ubyte)[]) s);
     digest.put(0);
 }
 
-void feedDigestData(D)(ref D digest, in string[] ss)
-if (isDigest!D)
+void feedDigestData(D)(ref D digest, in string[] ss) if (isDigest!D)
 {
     import std.bitmanip : nativeToLittleEndian;
 
-    digest.put(nativeToLittleEndian(cast(uint)ss.length));
-    foreach (s; ss) {
-        digest.put(cast(const(ubyte)[])s);
+    digest.put(nativeToLittleEndian(cast(uint) ss.length));
+    foreach (s; ss)
+    {
+        digest.put(cast(const(ubyte)[]) s);
         digest.put(0);
     }
 }
 
 void feedDigestData(D, V)(ref D digest, in V val)
-if (isDigest!D && (isIntegral!V || is(V == enum)))
+        if (isDigest!D && (isIntegral!V || is(V == enum)))
 {
     import std.bitmanip : nativeToLittleEndian;
 
-    digest.put(nativeToLittleEndian(cast(uint)val));
+    digest.put(nativeToLittleEndian(cast(uint) val));
     digest.put(0);
 }
 
@@ -65,9 +64,10 @@ struct FlagFile
 
     void remove()
     {
-        import std.file : remove;
+        import std.file : exists, remove;
 
-        remove(path);
+        if (exists(path))
+            remove(path);
     }
 
     @property auto timeLastModified()
@@ -91,15 +91,18 @@ string[] allEntries(string dir) @trusted
             .array).array;
 }
 
-
 string findProgram(in string name)
 {
     import std.process : environment;
-    version(Windows) {
+
+    version (Windows)
+    {
         import std.algorithm : endsWith;
+
         const efn = name.endsWith(".exe") ? name : name ~ ".exe";
     }
-    else {
+    else
+    {
         const efn = name;
     }
 
@@ -107,22 +110,27 @@ string findProgram(in string name)
 }
 
 /// environment variable path separator
-version(Posix) enum envPathSep = ':';
-else version(Windows) enum envPathSep = ';';
-else static assert(false);
+version (Posix)
+    enum envPathSep = ':';
+else version (Windows)
+    enum envPathSep = ';';
+else
+    static assert(false);
 
 /// Search for filename in the envPath variable content which can
 /// contain multiple paths separated with sep depending on platform.
 /// Returns: null if the file can't be found.
-string searchInEnvPath(in string envPath, in string filename, in char sep=envPathSep)
+string searchInEnvPath(in string envPath, in string filename, in char sep = envPathSep)
 {
     import std.algorithm : splitter;
     import std.file : exists;
     import std.path : buildPath;
 
-    foreach (dir; splitter(envPath, sep)) {
+    foreach (dir; splitter(envPath, sep))
+    {
         const filePath = buildPath(dir, filename);
-        if (exists(filePath)) return filePath;
+        if (exists(filePath))
+            return filePath;
     }
     return null;
 }
@@ -130,7 +138,7 @@ string searchInEnvPath(in string envPath, in string filename, in char sep=envPat
 /// Search for filename pattern in the envPath variable content which can
 /// contain multiple paths separated with sep depending on platform.
 /// Returns: array of matching file names
-string[] searchPatternInEnvPath(in string envPath, in string pattern, in char sep=envPathSep) @trusted
+string[] searchPatternInEnvPath(in string envPath, in string pattern, in char sep = envPathSep) @trusted
 {
     import std.algorithm : map, splitter;
     import std.array : array;
@@ -138,47 +146,55 @@ string[] searchPatternInEnvPath(in string envPath, in string pattern, in char se
 
     string[] res = [];
 
-    foreach (dir; splitter(envPath, sep)) {
-        if (!exists(dir) || !isDir(dir)) continue;
-        res ~= dirEntries(dir, pattern, SpanMode.shallow)
-            .map!(de => de.name)
-            .array;
+    foreach (dir; splitter(envPath, sep))
+    {
+        if (!exists(dir) || !isDir(dir))
+            continue;
+        res ~= dirEntries(dir, pattern, SpanMode.shallow).map!(de => de.name).array;
     }
     return res;
 }
 
-void runCommand(in string[] command, string workDir = null, bool quiet = false, string[string] env = null) @trusted
+void runCommand(in string[] command, string workDir = null, bool quiet = false,
+        string[string] env = null) @trusted
 {
     runCommands((&command)[0 .. 1], workDir, quiet, env);
 }
 
-void runCommands(in string[][] commands, string workDir = null, bool quiet = false, string[string] env = null) @trusted
+void runCommands(in string[][] commands, string workDir = null, bool quiet = false,
+        string[string] env = null) @trusted
 {
     import std.conv : to;
     import std.exception : enforce;
     import std.process : Config, escapeShellCommand, Pid, spawnProcess, wait;
     import std.stdio : stdin, stdout, stderr, File;
 
-    version(Windows) enum nullFile = "NUL";
-    else version(Posix) enum nullFile = "/dev/null";
-    else static assert(0);
+    version (Windows)
+        enum nullFile = "NUL";
+    else version (Posix)
+        enum nullFile = "/dev/null";
+    else
+        static assert(0);
 
     auto childStdout = stdout;
     auto childStderr = stderr;
     auto config = Config.retainStdout | Config.retainStderr;
 
-    if (quiet) {
+    if (quiet)
+    {
         childStdout = File(nullFile, "w");
     }
 
-    foreach(cmd; commands){
-        if (!quiet) {
+    foreach (cmd; commands)
+    {
+        if (!quiet)
+        {
             stdout.writeln("running ", cmd.commandRep);
         }
         auto pid = spawnProcess(cmd, stdin, childStdout, childStderr, env, config, workDir);
         auto exitcode = pid.wait();
-        enforce(exitcode == 0, "Command failed with exit code "
-            ~ to!string(exitcode) ~ ": " ~ cmd.commandRep);
+        enforce(exitcode == 0,
+                "Command failed with exit code " ~ to!string(exitcode) ~ ": " ~ cmd.commandRep);
     }
 }
 
