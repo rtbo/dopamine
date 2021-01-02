ab0b148d048df60eb6d9446a97e231257621e7b6
Remi Thebault <remi.thebault@gmail.com>
2021-01-03 00:27:56 +0100

    FormatLogException

--- lib/src/dopamine/log.d
+++ lib/src/dopamine/log.d
@@ -74,7 +74,138 @@ void logError(Args...)(string msgf, Args args) @safe
     log(LogLevel.error, msgf, args);
 }
 
+/// Exception that formats its argument according a format string
+/// and that can also log itself to the [log] API.
+class FormatLogException : Exception
+{
+    LogLevel level = LogLevel.error;
+    string fmt;
+    Object[] values;
+
+    this(Args...)(string fmt, Args args)
+    {
+        super(format(fmt, args));
+        this.fmt = fmt;
+        static foreach (arg; args)
+        {
+            values ~= new TDynLogValue!(typeof(arg))(arg);
+        }
+    }
+
+    this(Args...)(LogLevel level, string fmt, Args args)
+    {
+        super(format(fmt, args));
+        this.level = level;
+        this.fmt = fmt;
+        static foreach (arg; args)
+        {
+            values ~= new TDynLogValue!(typeof(arg))(arg);
+        }
+    }
+
+
+    void log()
+    {
+        import std.exception : enforce;
+
+        if (level < minLogLevel)
+            return;
+
+        logging = true;
+        scope (exit)
+            logging = false;
+
+        size_t valI;
+        auto f = fmt;
+
+        while (f.length > 0)
+        {
+            if (f[0] == '%')
+            {
+                enforce(f.length > 1, "Invalid log format string: \"" ~ fmt ~ "\"");
+                if (f[1] == '%')
+                {
+                    instance.put("%");
+                    f = f[2 .. $];
+                    continue;
+                }
+                size_t len = 1;
+                while (!isEndOfSpec(f[len]))
+                {
+                    ++len;
+                    enforce(f.length > len, "Invalid log format string: \"" ~ fmt ~ "\"");
+                }
+                enforce(valI < values.length, "Orphean log format specifier");
+                auto val = cast(DynLogValue) values[valI];
+                const spec = singleSpec(f[0 .. len + 1]);
+                val.formatVal(spec);
+                f = f[len + 1 .. $];
+                valI++;
+            }
+            else
+            {
+                size_t len = 1;
+                while (f.length > len && f[len] != '%')
+                    len++;
+                instance.put(f[0 .. len]);
+                f = f[len .. $];
+            }
+        }
+        enforce(valI == values.length, "Orphean log format value");
+    }
+}
+
+@("FormatLogException")
+unittest
+{
+    auto oldInstance = instance;
+    scope (exit)
+        instance = oldInstance;
+
+    auto output = new TestLogOutput;
+    instance = output;
+
+    auto e = new FormatLogException(LogLevel.info, "Test %s and %s. fourty-two = %s",
+            ColorizedText(Color.green, "success"), ColorizedText(Color.red, "error"), 42);
+
+    enum expectedMsg = "Test success and error. fourty-two = 42";
+    enum expectedLog = "Test [green]success[reset] and [red]error[reset]. fourty-two = 42";
+
+    assert(e.message == expectedMsg);
+    assert(output.output.data == "");
+
+    e.log();
+
+    assert(e.message == expectedMsg);
+    assert(output.output.data == expectedLog);
+}
+
 private:
+
+bool isEndOfSpec(char c)
+{
+    switch (c)
+    {
+    case 's':
+    case 'c':
+    case 'b':
+    case 'd':
+    case 'o':
+    case 'x':
+    case 'X':
+    case 'e':
+    case 'E':
+    case 'f':
+    case 'F':
+    case 'g':
+    case 'G':
+    case 'a':
+    case 'A':
+        return true;
+    default:
+        return false;
+    }
+}
 
 LogOutput instance;
 bool logging; // true when currently logging through instance
@@ -186,5 +317,57 @@ class TerminalLogOutput : LogOutput
     override void flush()
     {
         term.flush();
+    }
+}
+
+version (unittest)
+{
+    class TestLogOutput : LogOutput
+    {
+        import std.array : Appender;
+
+        Appender!string output;
+
+        override void color(int col)
+        {
+            import std.conv : to;
+
+            auto c = cast(Color) col;
+            output.put("[" ~ c.to!string ~ "]");
+        }
+
+        override void reset()
+        {
+            output.put("[reset]");
+        }
+
+        override void put(const(char)[] msg)
+        {
+            output.put(msg);
+        }
+
+        override void flush()
+        {
+        }
+    }
+}
+
+abstract class DynLogValue
+{
+    abstract void formatVal(scope const ref FormatSpec!char spec);
+}
+
+class TDynLogValue(T) : DynLogValue
+{
+    T value;
+
+    this(T value)
+    {
+        this.value = value;
+    }
+
+    override void formatVal(scope const ref FormatSpec!char spec)
+    {
+        formatValue(instance, value, spec);
     }
 }
