3c4f7ee469c0f6cd399aa1ae5b4537ff36d28cc9
Remi Thebault <remi.thebault@gmail.com>
2021-01-09 18:28:27 +0100

    restart a few things over

--- example/src/c_cpp/header.hpp
+++
@@ -1,5 +0,0 @@
-
-namespace lib {
-    int calc_square(int x);
-    void print_root(double x);
-}
--- example/src/d/lib.d
+++
@@ -1,7 +0,0 @@
-module lib;
-
-extern(C)
-int lib_calc_square(int x);
-
-extern(C)
-void lib_print_root(double x);
