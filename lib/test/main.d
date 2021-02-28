78604fbd5be22cec71c7f81beac61e19ad349585
Remi Thebault <remi.thebault@gmail.com>
2021-02-28 10:29:20 +0100

    (disabled) auto discovery of unittest

--- lib/test/main.d
+++ lib/test/main.d
@@ -2,24 +2,8 @@ module test.main;
 
 import test.util : testPath;
 
-
-import dopamine.api.transport;
-import dopamine.depdag;
-import dopamine.dependency;
-import dopamine.deplock;
-import dopamine.log;
-import dopamine.login;
-import dopamine.lua.lib;
-import dopamine.lua.profile;
-import dopamine.paths;
-import dopamine.profile;
-import dopamine.semver;
-import dopamine.util;
-import test.recipe;
-
 import std.exception : enforce;
 import std.process : execute;
-import std.meta;
 
 
 shared static this()
@@ -43,19 +27,3 @@ int main()
 {
     return 0;
 }
-
-alias allModules = AliasSeq!(
-    dopamine.api.transport,
-    dopamine.depdag,
-    dopamine.dependency,
-    dopamine.deplock,
-    dopamine.log,
-    dopamine.login,
-    dopamine.lua.lib,
-    dopamine.lua.profile,
-    dopamine.paths,
-    dopamine.profile,
-    dopamine.semver,
-    dopamine.util,
-    test.recipe,
-);
