4f88d83a26540e8b5b74f6539f8fb2f8ea7c8d70
Remi Thebault <remi.thebault@gmail.com>
2021-02-28 22:48:06 +0100

    test app works with pkgconfig

--- lib/test/data/pkgc/dopamine.lua
+++ lib/test/data/pkgc/dopamine.lua
@@ -17,9 +17,32 @@ function build(dirs, config, depinfos)
     }
 end
 
-function pack(dirs, config, dest)
-    local install = dop.installer('.', dest)
+function pack(dirs, config, depinfos)
+    local install = dop.installer('.', dirs.dest)
 
     install.file('libpkgc.a', 'lib/libpkgc.a');
     install.file('pkgc.d', 'include/d/pkgc-'..version..'/pkgc.d')
+
+    local pcdir = dop.path(dirs.dest, 'lib', 'pkgconfig')
+    local libdir = dop.path(dirs.dest, 'lib')
+    local libdir = dop.path(dirs.dest, 'include')
+
+    -- pkgapref can very well equal dirs.dest
+    local pkgapref = depinfos.pkga.install_dir
+
+    dop.mkdir{
+        pcdir, recurse=true
+    }
+    local pc = io.open(dop.path(pcdir, 'pkgc.pc'), 'w')
+    pc:write('prefix=', dirs.dest, '\n')
+    pc:write('libdir=${prefix}/lib\n')
+    pc:write('includedir=${prefix}/include\n')
+    pc:write('pkga_prefix=', pkgapref, '\n')
+    pc:write('\n')
+    pc:write('Name: ', name, '\n')
+    pc:write('Description: ', description, '\n')
+    pc:write('Version: ', version, '\n')
+    pc:write('Libs: -L${libdir} -lpkgc -L${pkga_prefix} -lpkga', '\n')
+    pc:write('Cflags: -I${includedir}/d/pkgc-'..version, '\n')
+    pc:close()
 end
