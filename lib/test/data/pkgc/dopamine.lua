94b125ffe0ee87d26cbeb6f62dc3cc53e8faa327
Remi Thebault <remi.thebault@gmail.com>
2021-02-28 21:30:56 +0100

    no need for issuing pkg-config

--- lib/test/data/pkgc/dopamine.lua
+++ lib/test/data/pkgc/dopamine.lua
@@ -22,23 +22,4 @@ function pack(dirs, config, dest)
 
     install.file('libpkgc.a', 'lib/libpkgc.a');
     install.file('pkgc.d', 'include/d/pkgc-'..version..'/pkgc.d')
-
-    pcdir = dop.path(dest, 'lib', 'pkgconfig')
-    libdir = dop.path(dest, 'lib')
-    libdir = dop.path(dest, 'include')
-
-    dop.mkdir{
-        pcdir, recurse=true
-    }
-    pc = io.open(dop.path(pcdir, 'pkgc.pc'), 'w')
-    pc:write('prefix=', dest, '\n')
-    pc:write('libdir=${prefix}/lib\n')
-    pc:write('includedir=${prefix}/include\n')
-    pc:write('\n')
-    pc:write('Name: ', name, '\n')
-    pc:write('Description: ', description, '\n')
-    pc:write('Version: ', version, '\n')
-    pc:write('Libs: -L${libdir} -lpkgc', '\n')
-    pc:write('Dflags: -I${includedir}/d/pkgc-'..version, '\n')
-    pc:close()
 end
