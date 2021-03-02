a81ae31fb06293843d29281cae14a93729836e4e
Remi Thebault <remi.thebault@gmail.com>
2021-03-02 22:41:50 +0100

    check content of /etc/ld.so.conf

--- lib/test/main.d
+++ lib/test/main.d
@@ -4,21 +4,57 @@ import test.util : testPath;
 
 import std.exception : enforce;
 import std.process : execute;
+import std.file;
 
 
 shared static this()
 {
     import dopamine.log : minLogLevel, LogLevel;
 
-    minLogLevel = LogLevel.error;
-
     enforce(execute(["dub", "add-path", testPath("pkgs")]).status == 0);
+    printEtcLdSoConf();
 }
 
 shared static ~this()
 {
     enforce(execute(["dub", "remove-path", testPath("pkgs")]).status == 0);
+    printEtcLdSoConf();
 }
+
+void printEtcLdSoConf()
+{
+    import std.stdio : writefln;
+
+    const conf = "/etc/ld.so.conf";
+    const confd = "/etc/ld.so.conf.d";
+
+    void catFile(string fn)
+    {
+        if (fn.exists && fn.isFile)
+        {
+            const content = cast(const(char)[])read(fn);
+            writefln("%s:", fn);
+            writefln("%s", content);
+        }
+        else
+        {
+            writefln("%s: No such file", fn);
+        }
+    }
+
+    void catDir(string dn)
+    {
+        foreach(e; dirEntries(dn, SpanMode.breadth))
+        {
+            if (e.isDir) catDir(e.name);
+            else catFile(e.name);
+        }
+    }
+
+    catFile(conf);
+    catDir(confd);
+}
+
 
 int main()
 {
