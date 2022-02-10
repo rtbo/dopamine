eb951719049e341e3ffa940b70e975310c7cb395
Remi Thebault <remi.thebault@gmail.com>
2022-02-10 23:53:53 +0100

    introduce end to end sandbox

--- test/end2end/e2e_driver.d
+++ test/end2end/e2e_driver.d
@@ -1,29 +1,163 @@
 module e2e_driver;
 
+import std.exception;
 import std.file;
 import std.path;
 import std.process;
 import std.regex;
 import std.stdio;
 import std.string;
-import std.typecons;
 
 struct Test
 {
-    string cmd;
-    string dir;
+    string name;
+    string command;
+    string directory;
     bool expectFail;
     string[] expectedOutputs;
-}
 
-string e2eDir()
-{
-    return buildNormalizedPath(dirName(__FILE_FULL_PATH__));
-}
+    static Test parseFile(string filename)
+    {
+        import std.typecons : No;
 
-string getLocalPath(string path)
-{
-    return isAbsolute(path) ? buildNormalizedPath(path) : buildNormalizedPath(getcwd(), path);
+        Test test;
+        test.name = baseName(stripExtension(filename));
+
+        auto testFile = File(filename, "r");
+
+        foreach (line; testFile.byLineCopy(No.keepTerminator))
+        {
+            line = line.strip();
+
+            if (line.startsWith("#"))
+            {
+                continue;
+            }
+            else if (line.startsWith("CMD="))
+            {
+                test.command = line[4 .. $];
+            }
+            else if (line.startsWith("DIR="))
+            {
+                test.directory = line[4 .. $];
+            }
+            else if (line == "EXPECT_FAIL")
+            {
+                test.expectFail = true;
+            }
+            else if (line.startsWith("EXPECT_OUTPUT="))
+            {
+                enum len = "EXPECT_OUTPUT=".length;
+                test.expectedOutputs ~= line[len .. $];
+            }
+            else
+            {
+                throw new Exception(format("%s: Unrecognized input: %s", test.name, line));
+            }
+        }
+
+        return test;
+    }
+
+    void check()
+    {
+        enforce(command, format("%s: CMD must be provided", name));
+        enforce(directory, format("%s: DIR must be provided", name));
+    }
+
+    File lockSandbox()
+    {
+        string path = sandboxPath(setExtension(directory, "lock"));
+        mkdirRecurse(dirName(path));
+        auto lock = File(path, "w");
+        lock.lock();
+        return lock;
+    }
+
+    void prepareSandbox()
+    {
+        string e2e = e2ePath();
+
+        foreach (entry; dirEntries(e2ePath(directory), SpanMode.breadth))
+        {
+            string radical = relativePath(entry.name, e2e);
+            string dest = sandboxPath(radical);
+
+            if (entry.isDir)
+            {
+                mkdirRecurse(dest);
+            }
+            else
+            {
+                copy(entry.name, dest);
+            }
+        }
+    }
+
+    void perform(string dopExe)
+    {
+        import std.array : Appender;
+
+        chdir(sandboxPath(directory));
+
+        const quotDop = format(`"%s"`, dopExe);
+        auto cmd = this.command
+            .replace("\"$DOP\"", quotDop)
+            .replace("$DOP", quotDop);
+
+        auto result = executeShell(cmd);
+
+        const fail = result.status != 0;
+
+        if (expectFail != fail)
+        {
+            Appender!string app;
+            if (expectFail)
+            {
+                app.put(format("%s: expected to fail but succeeded\n", name));
+            }
+            else
+            {
+                app.put(format("%s: command failed with status %s\n", name, result.status));
+            }
+            app.put("Output:\n");
+            app.put(result.output);
+            throw new Exception(app.data);
+        }
+
+        Appender!string failureMsg;
+        bool outputAdded;
+
+        foreach (exp; expectedOutputs)
+        {
+            auto res = matchFirst(result.output, exp);
+            if (res.empty)
+            {
+                if (!outputAdded)
+                {
+                    auto lineSplit = result.output.endsWith("\n") ? "" : "\n";
+                    failureMsg.put(format("%s FAILURE\n", name));
+                    failureMsg.put(format("command: %s\n", cmd));
+                    failureMsg.put("output:\n");
+                    failureMsg.put("-------\n");
+                    failureMsg.put(format("%s%s", result.output, lineSplit));
+                    failureMsg.put("-------\n");
+                    outputAdded = true;
+                }
+                failureMsg.put(format("Could not match %s", exp));
+            }
+        }
+
+        if (failureMsg.data.length)
+        {
+            throw new Exception(failureMsg.data);
+        }
+    }
+
+    void cleanup()
+    {
+        rmdirRecurse(sandboxPath(directory));
+    }
 }
 
 int main(string[] args)
@@ -34,105 +168,38 @@ int main(string[] args)
         return -1;
     }
 
-    const dopExe = getLocalPath(environment["DOP"]);
-    const testFileName = getLocalPath(args[1]);
-    const testName = baseName(stripExtension(testFileName));
+    const dopExe = absolutePath(environment["DOP"]);
 
-    auto testFile = File(testFileName, "r");
-
-    Test test;
-
-    foreach (line; testFile.byLineCopy(No.keepTerminator))
+    try
     {
-        line = line.strip();
+        auto test = Test.parseFile(args[1]);
+        test.check();
 
-        if (line.startsWith("#"))
-        {
-            continue;
-        }
-        else if (line.startsWith("CMD="))
-        {
-            test.cmd = line[4 .. $];
-        }
-        else if (line.startsWith("DIR="))
-        {
-            test.dir = line[4 .. $];
-        }
-        else if (line == "EXPECT_FAIL")
-        {
-            test.expectFail = true;
-        }
-        else if (line.startsWith("EXPECT_OUTPUT="))
-        {
-            enum len = "EXPECT_OUTPUT=".length;
-            test.expectedOutputs ~= line[len .. $];
-        }
-        else
-        {
-            stderr.writeln("Unrecognized test input: ", line);
-            return -2;
-        }
+        File lock = test.lockSandbox();
+        scope (exit)
+            lock.close();
+
+        test.prepareSandbox();
+        test.perform(dopExe);
+
+        test.cleanup();
     }
-
-    testFile.close();
-
-    if (!test.cmd)
+    catch (Exception ex)
     {
-        stderr.writefln("No command provided in  %s", testFileName);
-        return -2;
-    }
-
-    if (test.dir)
-    {
-        const dir = buildNormalizedPath(e2eDir, test.dir);
-        if (!exists(dir) || !isDir(dir))
-        {
-            stderr.writefln("No such directory: %s", dir);
-            return -3;
-        }
-        chdir(dir);
-    }
-
-    const quotDop = format(`"%s"`, dopExe);
-    auto cmd = test.cmd
-        .replace("\"$DOP\"", quotDop)
-        .replace("$DOP", quotDop);
-    auto result = executeShell(cmd);
-
-    const fail = result.status != 0;
-
-    if (test.expectFail != fail)
-    {
-        if (test.expectFail)
-        {
-            stderr.writefln("%s expected to fail but succeeded", testName);
-        }
-        else
-        {
-            stderr.writefln("%s expected to succeed but failed", testName);
-        }
-        stderr.writeln("Output:");
-        stderr.writeln(result.output);
+        stderr.writeln(ex.msg);
         return 1;
     }
 
-    int failures;
-    bool outputShown;
+    return 0;
 
-    foreach (exp; test.expectedOutputs)
-    {
-        auto res = matchFirst(result.output, exp);
-        if (res.empty)
-        {
-            if (!outputShown)
-            {
-                stderr.writefln("command: %s\noutput:\n%s\n--------", test.cmd, result.output);
-                outputShown = true;
-            }
-            stderr.writefln("Could not match %s", exp);
-            failures += 1;
-        }
-    }
+}
 
-    return failures;
+string e2ePath(Args...)(Args args)
+{
+    return buildNormalizedPath(dirName(__FILE_FULL_PATH__), args);
+}
+
+string sandboxPath(Args...)(Args args)
+{
+    return e2ePath("sandbox", args);
 }
