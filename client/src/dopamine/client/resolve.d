0cf1cbebcf546dfa80760d1f3aa5e536ffbc8929
Remi Thebault <remi.thebault@gmail.com>
2022-02-17 01:34:36 +0100

    resolve command

--- client/src/dopamine/client/resolve.d
+++ client/src/dopamine/client/resolve.d
@@ -3,9 +3,12 @@ module dopamine.client.resolve;
 import dopamine.client.utils;
 
 import dopamine.dep.dag;
+import dopamine.dep.service;
 import dopamine.log;
 import dopamine.paths;
+import dopamine.profile;
 
+import std.exception;
 import std.getopt;
 import std.stdio;
 import std.typecons;
@@ -22,7 +25,7 @@ int resolveMain(string[] args)
 
     auto helpInfo = getopt(args,
         "force|f", "Resolve dependencies and overwrite lock file", &force,
-        "prefer-system", "Resolve dependencies using the `preferSystem` mode", &preferSystem,
+        "prefer-system", "Resolve dependencies using the `preferSystem` mode (Default)", &preferSystem,
         "prefer-cache", "Resolve dependencies using the `preferCache` mode", &preferCache,
         "prefer-local", "Resolve dependencies using the `preferLocal` mode", &preferLocal,
         "pick-highest", "Resolve dependencies using the `pickHighest` mode", &pickHighest,
@@ -45,19 +48,65 @@ int resolveMain(string[] args)
         return 0;
     }
 
+    enforce(dir.hasProfileFile, new ErrorLogException(
+            "A compilation profile is needed to resolve dependencies. You may try %s.",
+            info("dop profile default")
+        )
+    );
+    auto profile = Profile.loadFromFile(dir.profileFile);
+
+    // FIXME: add options to modify existing lock file
+
     if (dir.hasLockFile && !force)
     {
-        logError(
-            "%s %s already exist, use %s to overwrite",
-            error("Error:"), dir.lockFile, info("--force")
+        throw new ErrorLogException(
+            "%s already exist, use %s to overwrite", dir.lockFile, info("--force")
         );
-        return 1;
     }
 
     const network = noNetwork ? No.network : Yes.network;
     const system = noSystem ? No.system : Yes.system;
 
+    auto service = new DependencyService(network, system);
 
+    Heuristics heuristics;
+    heuristics.mode = heuristicsMode(preferSystem, preferCache, preferLocal, pickHighest);
+
+    // TODO: system block-list/enable-list
+
+    auto dag = DepDAG.prepare(recipe, profile, service, heuristics);
+    dag.resolve();
+
+    auto json = dag.toJson();
+
+    import std.file : write;
+    write(dir.lockFile, json.toPrettyString());
 
     return 0;
+}
+
+private Heuristics.Mode heuristicsMode(bool preferSystem, bool preferCache, bool preferLocal, bool pickHighest)
+{
+    import std.algorithm;
+    import std.range : only;
+
+    int count = only(preferSystem, preferCache, preferLocal, pickHighest).map!(
+            b => cast(int) b).sum();
+
+    enforce(count <= 1, new ErrorLogException("Only one resolution mode must be supplied!"));
+
+    if (preferSystem) {
+        return Heuristics.Mode.preferSystem;
+    }
+    if (preferCache) {
+        return Heuristics.Mode.preferCache;
+    }
+    if (preferLocal) {
+        return Heuristics.Mode.preferLocal;
+    }
+    if (pickHighest) {
+        return Heuristics.Mode.pickHighest;
+    }
+
+    return Heuristics.Mode.preferSystem;
 }
