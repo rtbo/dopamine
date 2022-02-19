3f040d6749e88d9f90a043ba886350f46eb43157
Remi Thebault <remi.thebault@gmail.com>
2022-02-19 15:15:26 +0100

    update e2e registry

--- test/end2end/e2e_registry.d
+++ test/end2end/e2e_registry.d
@@ -1,9 +1,11 @@
 module e2e_registry;
 
+import vibe.core.core;
 import vibe.http.router;
 import vibe.http.server;
 
 import std.conv;
+import std.stdio;
 
 void packages(HTTPServerRequest req, HTTPServerResponse res)
 {}
@@ -11,10 +13,10 @@ void packages(HTTPServerRequest req, HTTPServerResponse res)
 void packageVersions(HTTPServerRequest req, HTTPServerResponse res)
 {}
 
-void main(string args[])
+void main(string[] args)
 {
     ushort port = 3500;
-    if (args >= 1)
+    if (args.length >= 2)
     {
         port = args[1].to!ushort;
     }
@@ -26,7 +28,9 @@ void main(string args[])
 
     auto router = new URLRouter("/api/v1");
     router.get("/packages", &packages);
-    router.get("/packages/:pack/versions", packageVersions);
+    router.get("/packages/:pack/versions", &packageVersions);
 
     listenHTTP(settings, router);
+
+    runApplication();
 }
