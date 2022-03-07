dba0ed3bf1229a26c4b22de8ada15b4821dee869
Remi Thebault <remi.thebault@gmail.com>
2022-03-07 18:28:54 +0100

    check recipes in-tree

--- client/src/dopamine/client/source.d
+++ client/src/dopamine/client/source.d
@@ -2,12 +2,20 @@ module dopamine.client.source;
 
 import dopamine.client.utils;
 
+import dopamine.log;
 import dopamine.paths;
+
+import std.path;
 
 int sourceMain(string[] args)
 {
     const dir = RecipeDir.enforced(".");
     auto recipe = parseRecipe(dir);
+
+    if (recipe.inTreeSrc)
+    {
+        logInfo("%s: in-tree at %s - nothing to do", info("Source"), info(absolutePath(recipe.source())));
+    }
 
     return 0;
 }
