9820742047e1a963c387b0108283fe85c7edcbf7
Remi Thebault <remi.thebault@gmail.com>
2022-03-14 08:03:59 +0100

    update lib1 recipe

--- test/end2end/recipes/lib1/dopamine.lua
+++ test/end2end/recipes/lib1/dopamine.lua
@@ -4,11 +4,12 @@ return {
     langs = {'c'},
     revision = '1',
 
-    build = function(self, dirs, config)
-        local profile = config.profile
-        local meson = dop.Meson:new(profile)
+    -- called from the config directory
+    build = function(self, src_dir, config)
+        local meson = dop.Meson:new(config.profile)
 
-        dop.mkdir {dirs.build, recurse = true}
+        build_dir = dop.mkdir {"build", recurse = true}
+
 
         meson:setup{build_dir = dirs.build, install_dir = dirs.install}
         dop.from_dir(dirs.build, function()
