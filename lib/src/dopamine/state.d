c3c2677e1906849ff49cc2fae2fe01f2f25749b7
Remi Thebault <remi.thebault@gmail.com>
2022-04-07 19:25:26 +0200

    checkSourceReady

--- lib/src/dopamine/state.d
+++ lib/src/dopamine/state.d
@@ -1,6 +1,7 @@
 module dopamine.state;
 
 import dopamine.paths;
+import dopamine.recipe;
 import dopamine.util;
 
 /// Content of the main state for the package dir state
@@ -14,6 +15,31 @@ alias PkgStateFile = JsonStateFile!PkgState;
 @property PkgStateFile stateFile(const RecipeDir rdir)
 {
     return PkgStateFile(rdir._dopPath("state.json"));
+}
+
+
+string checkSourceReady(RecipeDir dir, Recipe recipe, out string reason)
+{
+    if (recipe.inTreeSrc)
+    {
+        const srcDir = recipe.source();
+        return srcDir;
+    }
+
+    auto sf = dir.stateFile;
+    auto state = sf.read();
+    if (!sf || !state.srcDir)
+    {
+        reason = "Source directory is not ready";
+        return null;
+    }
+
+    if (sf.timeLastModified < dir.recipeLastModified)
+    {
+        reason = "Source directory is not up-to-date";
+        return null;
+    }
+    return state.srcDir;
 }
 
 /// Content of the state relative to a build configuration
