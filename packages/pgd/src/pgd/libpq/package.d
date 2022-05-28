b7a2a3b965ad0af8c45e2f8355389fcc755b36d8
Remi Thebault <remi.thebault@gmail.com>
2022-05-28 17:25:10 +0200

    add OID bindings

--- packages/pgd/src/pgd/libpq/package.d
+++ packages/pgd/src/pgd/libpq/package.d
@@ -4,9 +4,38 @@ module pgd.libpq;
 public import pgd.libpq.bindings;
 public import pgd.libpq.defs;
 
+// libpq-fe.h
 PGconn* PQsetdb(const(char)* pghost, const(char)* pgport, const(char)* pgoptions,
     const(char)* pgtty, const(char)* dbName)
 {
     pragma(inline, true)
     return PQsetdbLogin(pghost, pgport, pgoptions, pgtty, dbName, null, null);
+}
+
+// pg_type_d.h
+
+/* Is a type OID a polymorphic pseudotype?	(Beware of multiple evaluation) */
+bool IsPolymorphicType(Oid typid)
+{
+    return IsPolymorphicTypeFamily1(typid) || IsPolymorphicTypeFamily2(typid);
+}
+
+/* Code not part of polymorphic type resolution should not use these macros: */
+bool IsPolymorphicTypeFamily1(Oid typid)
+{
+    return typid == ANYELEMENTOID ||
+        typid == ANYARRAYOID ||
+        typid == ANYNONARRAYOID ||
+        typid == ANYENUMOID ||
+        typid == ANYRANGEOID ||
+        typid == ANYMULTIRANGEOID;
+}
+
+bool IsPolymorphicTypeFamily2(Oid typid)
+{
+    return typid == ANYCOMPATIBLEOID ||
+        typid == ANYCOMPATIBLEARRAYOID ||
+        typid == ANYCOMPATIBLENONARRAYOID ||
+        typid == ANYCOMPATIBLERANGEOID ||
+        typid == ANYCOMPATIBLEMULTIRANGEOID;
 }
