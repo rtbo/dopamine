a7816f2195c1bae2a16af308145f4357305f1a8b
Remi Thebault <remi.thebault@gmail.com>
2022-05-28 17:03:00 +0200

    typed result API

--- packages/pgd/src/pgd/conn.d
+++ packages/pgd/src/pgd/conn.d
@@ -3,11 +3,30 @@ module pgd.conn;
 import pgd.libpq;
 import pgd.result;
 
+import std.array;
 import std.conv;
+import std.exception;
+import std.meta;
 import std.string;
 import std.traits;
 
-enum isDbScalar(T) = isSomeString!T || isIntegral!T || isFloatingPoint!T;
+enum isScalar(T) = isSomeString!T || isIntegral!T || isFloatingPoint!T;
+enum isRow(R) = is(R == struct);
+
+/// struct attached as UDA to provide at compile time
+/// the column index in a query result.
+/// If not supplied, the column index is fetched at run time from the member name.
+struct ColInd
+{
+    int ind;
+}
+
+/// struct attached as UDA to provide at compile time
+/// an alternative name for the lookup of column index in the query result
+struct ColName
+{
+    string name;
+}
 
 /// An interface to a Postgresql connection.
 /// This interface is single threaded and cannot be shared among threads.
@@ -56,23 +75,6 @@ class PgConn
         conn.pgEnforce(PQsetSingleRowMode(conn) == 1);
     }
 
-    /// Execute a SQL statement expecting no result.
-    /// Thread is blocked until response is received from the server
-    void execSync(Args...)(string sql, Args args)
-    {
-        sendPriv(sql, args);
-
-        auto res = PQgetResult(conn);
-        conn.pgEnforce(res && (PQresultStatus(res) == ExecStatus.COMMAND_OK));
-
-        // drain results (should be a single null at this point)
-        while (res)
-        {
-            PQclear(res);
-            res = PQgetResult(conn);
-        }
-    }
-
     private void sendPriv(Args...)(string sql, Args args)
     {
         static if (Args.length > 0)
@@ -92,12 +94,138 @@ class PgConn
         conn.pgEnforce(res == 1);
     }
 
-    private PGresult* execPriv(Args...)(string sql, Args args)
+    /// Execute a SQL statement expecting no result.
+    /// Thread is blocked until response is received from the server
+    void execSync(Args...)(string sql, Args args)
     {
-        static if (Args.length > 0)
+        sendPriv(sql, args);
+
+        auto res = PQgetResult(conn);
+
+        scope (exit)
+            clearAndDrain(res);
+
+        conn.pgEnforce(res && (PQresultStatus(res) == ExecStatus.COMMAND_OK));
+    }
+
+    T execScalarSync(T, Args...)(string sql, Args args) if (isScalar!T)
+    {
+        sendPriv(sql, args);
+
+        auto res = PQgetResult(conn);
+
+        scope (exit)
+            clearAndDrain(res);
+
+        const status = PQresultStatus(res);
+        if (status.isError)
         {
-            // TODO: debug check that maximum index in sql is not greater than args.length
-            auto params = pgQueryParams(args);
+            throw new Exception(formatQueryMsg(res, sql, args));
+        }
+
+        enforce(PQnfields(res) == 1, "Expected a single column result");
+        enforce(PQntuples(res) == 1, "Expected a single row result");
+
+        return convScalar!T(0, 0, res);
+    }
+
+    T[] execScalarsSync(T, Args...)(string sql, Args args) if (isScalar!T)
+    {
+        sendPriv(sql, args);
+
+        auto res = PQgetResult(conn);
+
+        scope (exit)
+            clearAndDrain(res);
+
+        const status = PQresultStatus(res);
+        if (status != ExecStatus.TUPLES_OK)
+        {
+            // FIXME: find out if error or if void and define exception types
+            throw new Exception("FIXME");
+        }
+        // FIXME: throw if multiple columns
+
+        const nrows = PQntuples(res);
+        if (!nrows)
+            return [];
+
+        T[] scalars = uninitializedArray!(T[])(nrows);
+        foreach (ri; 0 .. nrows)
+        {
+            scalars[ri] = convScalar!T(ri, 0, res);
+        }
+
+        return scalars;
+    }
+
+    /// Execute a SQL statement expecting a single row result.
+    /// Result row is converted to the provided struct type.
+    R execRowSync(R, Args...)(string sql, Args args) if (isRow!R)
+    {
+        sendPriv(sql, args);
+
+        auto res = PQgetResult(conn);
+
+        scope (exit)
+            clearAndDrain(res);
+
+        const status = PQresultStatus(res);
+        if (status != ExecStatus.TUPLES_OK)
+        {
+            // FIXME: find out if error or if void and define exception types
+            throw new Exception("FIXME");
+        }
+
+        // throw if multiple rows
+
+        auto colInds = getColIndices!R(res);
+        auto row = convRow!R(colInds, 0, res);
+
+        return row;
+    }
+
+    /// Execute a SQL statement expecting a zero or many row result.
+    /// Result rows are converted to the provided struct type.
+    R[] execRowsSync(R, Args...)(string sql, Args args) if (isRow!R)
+    {
+        sendPriv(sql, args);
+
+        auto res = PQgetResult(conn);
+
+        scope (exit)
+            clearAndDrain(res);
+
+        const status = PQresultStatus(res);
+        if (status != ExecStatus.TUPLES_OK)
+        {
+            // FIXME: find out if error or if void and define exception types
+            throw new Exception("FIXME");
+        }
+
+        const nrows = PQntuples(res);
+        if (!nrows)
+            return [];
+
+        auto colInds = getColIndices!R(res);
+
+        R[] rows = uninitializedArray!(R[])(nrows);
+        foreach (ri; 0 .. nrows)
+        {
+            rows[ri] = convRow!R(colInds, ri, res);
+        }
+
+        return rows;
+    }
+
+    private void clearAndDrain(PGresult* res)
+    {
+        while (res)
+        {
+            PQclear(res);
+            res = PQgetResult(conn);
+        }
+    }
 
     /// Get the (untyped) result for the current query
     /// if result casts to false, it means there is no more result
@@ -218,6 +346,36 @@ auto pgEnforce(C)(PGconn* conn, C cond)
     return cond;
 }
 
+@property bool isError(ExecStatus status)
+{
+    switch(status)
+    {
+    case ExecStatus.EMPTY_QUERY:
+    case ExecStatus.BAD_RESPONSE:
+    case ExecStatus.NONFATAL_ERROR:
+    case ExecStatus.FATAL_ERROR:
+        return true;
+    default:
+        return false;
+    }
+}
+
+string formatQueryMsg(Args...)(PGresult* res, string sql, Args args)
+{
+    string msg = "Error during query execution.\n";
+    msg ~= "SQL:\n  " ~ sql ~ "\n";
+    if (args.length)
+    {
+        msg ~= "Params:\n";
+        static foreach (i, arg; args)
+            msg ~= format("  $%s = %s\n", i + 1, arg);
+    }
+    const pgMsg = PQresultErrorMessage(res).fromStringz;
+    msg ~= "PostgreSQL message: " ~ pgMsg;
+    return msg ~ "\n";
+}
+
+/// Bind query args to parameters to PQexecParams or similar
 struct PgQueryParams
 {
     const(char)*[] values;
@@ -236,6 +394,7 @@ struct PgQueryParams
     }
 }
 
+/// ditto
 PgQueryParams pgQueryParams(Args...)(Args args)
 {
     PgQueryParams params;
@@ -243,9 +402,114 @@ PgQueryParams pgQueryParams(Args...)(Args args)
 
     static foreach (i, arg; args)
     {
-        string val = arg.to!string;
-        params.values[i] = val.ptr;
-        params.lengths[i] = cast(int) val.length;
+        {
+            string val = arg.to!string;
+            params.values[i] = val.toStringz();
+            params.lengths[i] = cast(int) val.length;
+        }
     }
     return params;
 }
+
+/// Generate a struct that has integer fields
+/// of the same name of the provided row type.
+/// Used to cache indices of the columns
+string generateColIndexStruct(R)() if (isRow!R)
+{
+    string res = "static struct _ColIndices {\n";
+
+    static foreach (f; FieldNameTuple!R)
+    {
+        res ~= "    int " ~ f ~ ";\n";
+    }
+
+    res ~= "}";
+    return res;
+}
+
+/// Generate a struct that has string fields
+/// of the same name of the provided row type.
+/// Used to cache customized names of the columns
+string generateColNameStruct(R)() if (isRow!R)
+{
+    string res = "static struct _ColNames {\n";
+
+    static foreach (f; FieldNameTuple!R)
+    {
+        res ~= "    string " ~ f ~ ";\n";
+    }
+
+    res ~= "}";
+    return res;
+}
+
+auto getColIndices(R)(const(PGresult)* res)
+{
+    mixin(generateColIndexStruct!R());
+    mixin(generateColNameStruct!R());
+
+    _ColIndices inds = void;
+    _ColNames names = void;
+
+    static foreach (f; FieldNameTuple!R)
+    {
+        __traits(getMember, inds, f) = -1;
+        __traits(getMember, names, f) = f;
+    }
+
+    alias colIndUDAs = getSymbolsByUDA!(R, ColInd);
+    static foreach (indUDA; colIndUDAs)
+    {
+        __traits(getMember, inds, indUDA.stringof) = getUDAs!(indUDA, ColInd)[0].ind;
+    }
+
+    alias colNameUDAs = getSymbolsByUDA!(R, ColName);
+    static foreach (nameUDA; colNameUDAs)
+    {
+        __traits(getMember, names, nameUDA.stringof) = getUDAs!(nameUDA, ColName)[0].name;
+    }
+
+    int ncols = PQnfields(res);
+
+    // dfmt off
+    static foreach (f; FieldNameTuple!R)
+    {{
+        if (__traits(getMember, inds, f) == -1)
+        {
+            const name = __traits(getMember, names, f);
+            __traits(getMember, inds, f) = PQfnumber(res, name.toStringz);
+        }
+
+        const ind = __traits(getMember, inds, f);
+        enforce(ind >= 0 && ind < ncols, "Cannot lookup column index for " ~ R.stringof ~ "." ~ f);
+    }}
+    // dfmt on
+
+    return inds;
+}
+
+T convScalar(T)(int rowInd, int colInd, const(PGresult)* res)
+{
+    const len = PQgetlength(res, rowInd, colInd);
+    const pval = PQgetvalue(res, rowInd, colInd);
+    const val = pval[0 .. len];
+    return val.to!T;
+}
+
+R convRow(R, CI)(CI colInds, int rowInd, const(PGresult)* res)
+{
+    R row = void;
+    static foreach (f; FieldNameTuple!R)
+    {
+        {
+            const colInd = __traits(getMember, colInds, f);
+            const len = PQgetlength(res, rowInd, colInd);
+            const pval = PQgetvalue(res, rowInd, colInd);
+            const val = pval[0 .. len];
+            __traits(getMember, row, f) = val.to!(typeof(__traits(getMember, row, f)));
+        }
+    }
+    return row;
+}
+
+/// TODO: make dummy connection and dummy result for testing
