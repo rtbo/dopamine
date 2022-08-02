4d526e99f71139cd1874471775f1e9ed4f344d8f
Remi Thebault <remi.thebault@gmail.com>
2022-08-02 14:49:58 +0200

    server is setup to serve the frontend

--- packages/server/src/dopamine/server/app.d
+++ packages/server/src/dopamine/server/app.d
@@ -10,11 +10,15 @@ import cors_vibe;
 
 import vibe.core.core;
 import vibe.core.log;
+import vibe.http.fileserver;
 import vibe.http.router;
 import vibe.http.server;
 import vibe.stream.tls;
 
+import std.file;
 import std.format;
+import std.path;
+import std.string;
 
 version (DopServerMain) void main(string[] args)
 {
@@ -32,7 +36,9 @@ class DopRegistry
 {
     DbClient client;
     HTTPServerSettings settings;
-    URLRouter router;
+
+    URLRouter root;
+    URLRouter api;
 
     this()
     {
@@ -49,47 +55,73 @@ class DopRegistry
             settings.tlsContext.usePrivateKeyFile(conf.httpsKey);
         }
 
+        api = new URLRouter("/api");
+        api.any("*", cors());
+
         auto auth = new AuthApi(client);
+        auth.setupRoutes(api);
+
         auto v1 = v1Api(client);
+        v1.setupRoutes(api);
 
-        const prefix = format("/api");
-        router = new URLRouter(prefix);
-        router.any("*", cors());
+        debug
+        {
+            if (conf.testStopRoute)
+                api.post("/stop", &stop);
+        }
 
-        auth.setupRoutes(router);
-        v1.setupRoutes(router);
+        root = new URLRouter;
+        root.any("/api/*", api);
 
-        if (conf.testStopRoute)
-            router.post("/stop", &stop);
+        // setup front-end
+        string publicFolder = thisExePath
+            .dirName
+            .dirName
+            .buildPath("share", "dopamine", "public")
+            .buildNormalizedPath();
 
-        router.get("*", &fallback);
+        debug
+        {
+            if (!exists(publicFolder))
+            {
+                publicFolder = __FILE_FULL_PATH__
+                    .dirName
+                    .dirName
+                    .dirName
+                    .dirName
+                    .dirName
+                    .dirName
+                    .buildPath("web", "dist")
+                    .buildNormalizedPath();
+            }
+        }
+
+        logInfo("Serving front-end from %s", publicFolder);
+
+        root.get("/assets/*", serveStaticFiles(publicFolder));
+        root.get("/favicon.ico", serveStaticFile(buildPath(publicFolder, "favicon.ico")));
+        auto indexService = serveStaticFile(buildPath(publicFolder, "index.html"));
+        root.get("*", (scope req, scope resp) {
+            // missed api and asset requests yield 404
+            if (req.path.startsWith("/api/") || req.path.startsWith("/assets/"))
+                return;
+            // all other routes serve index.html to let vue-router do its job
+            indexService(req, resp);
+        });
     }
 
     HTTPListener listen()
     {
-        return listenHTTP(settings, &rootHandler);
+        return listenHTTP(settings, root);
     }
 
-    void rootHandler(scope HTTPServerRequest req, scope HTTPServerResponse resp)
+    debug
     {
-        if (req.path == "/" && req.method == HTTPMethod.GET) {
-            const conf = Config.get;
-            resp.redirect("http://" ~ conf.frontendOrigin);
-            return;
+        void stop(HTTPServerRequest req, HTTPServerResponse resp)
+        {
+            resp.writeBody("", 200);
+            client.finish();
+            exitEventLoop();
         }
-
-        router.handleRequest(req, resp);
-    }
-
-    void stop(HTTPServerRequest req, HTTPServerResponse resp)
-    {
-        resp.writeBody("", 200);
-        client.finish();
-        exitEventLoop();
-    }
-
-    void fallback(HTTPServerRequest req, HTTPServerResponse resp)
-    {
-        logInfo("fallback for %s", req.requestURI);
     }
 }
