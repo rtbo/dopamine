dafc39c74b1a2324a4c4d12773e86e44c3c8118e
Remi Thebault <remi.thebault@gmail.com>
2022-08-06 18:19:01 +0200

    pgd assembleConnString

--- packages/pgd/src/pgd/connstring.d
+++ packages/pgd/src/pgd/connstring.d
@@ -2,6 +2,7 @@ module pgd.connstring;
 
 import pgd.libpq;
 
+import std.format;
 import std.string;
 
 @safe:
@@ -31,34 +32,67 @@ string[string] breakdownConnString(string conninfo) @trusted
     return res;
 }
 
+string assembleConnString(const(string[string]) connInfo)
+{
+    import std.algorithm : filter, map;
+    import std.array : join;
+
+    const user = connInfo.get("user", null);
+    const pswd = connInfo.get("password", null);
+    const userSpec = (user || pswd) ? format!"%s%s%s@"(user, pswd ? ":" : "", pswd) : "";
+
+    const host = connInfo.get("host", null);
+    const port = connInfo.get("port", null);
+    const hostSpec = (host || port) ? format!"%s%s%s"(host, port ? ":" : "", port) : "";
+
+    const dbname = connInfo.get("dbname", null);
+    const dbSpec = dbname ? format!"/%s"(dbname) : "";
+
+    const paramSpec = connInfo.byKeyValue()
+        .filter!(kv => kv.key != "user")
+        .filter!(kv => kv.key != "password")
+        .filter!(kv => kv.key != "host")
+        .filter!(kv => kv.key != "port")
+        .filter!(kv => kv.key != "dbname")
+        .map!(kv => format!"%s=%s"(kv.key, kv.value))
+        .join("&");
+
+    return format!"postgres://%s%s%s%s%s"(userSpec, hostSpec, dbSpec, paramSpec ? "?" : "", paramSpec);
+}
+
 version (unittest) import unit_threaded.assertions;
 
-@("breakdownConnString")
+@("breakdownConnString, assembleConnString")
 unittest
 {
-    const bd0 = breakdownConnString("postgres://");
-    const string[string] exp0;
+    const str0 = "postgres://";
+    const string[string] info0;
 
-    const bd1 = breakdownConnString("postgres:///adatabase");
-    const string[string] exp1 = ["dbname": "adatabase"];
+    const str1 = "postgres:///adatabase";
+    const string[string] info1 = ["dbname": "adatabase"];
 
-    const bd2 = breakdownConnString("postgres://somehost:3210/adatabase");
-    const string[string] exp2 = [
+    const str2 = "postgres://somehost:3210/adatabase";
+    const string[string] info2 = [
         "host": "somehost",
         "port": "3210",
         "dbname": "adatabase",
     ];
 
-    const bd3 = breakdownConnString("postgres://someuser@somehost:3210/adatabase");
-    const string[string] exp3 = [
+    const str3 = "postgres://someuser@somehost:3210/adatabase";
+    const string[string] info3 = [
         "user": "someuser",
         "host": "somehost",
         "port": "3210",
         "dbname": "adatabase",
     ];
 
-    bd0.shouldEqual(exp0);
-    bd1.shouldEqual(exp1);
-    bd2.shouldEqual(exp2);
-    bd3.shouldEqual(exp3);
+    info0.shouldEqual(breakdownConnString(str0));
+    info1.shouldEqual(breakdownConnString(str1));
+    info2.shouldEqual(breakdownConnString(str2));
+    info3.shouldEqual(breakdownConnString(str3));
+
+    str0.shouldEqual(assembleConnString(info0));
+    str1.shouldEqual(assembleConnString(info1));
+    str2.shouldEqual(assembleConnString(info2));
+    str3.shouldEqual(assembleConnString(info3));
 }
