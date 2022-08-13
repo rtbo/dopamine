55a1a69d453b8e45c8ccf4e321f3bc3754fb88c4
Remi Thebault <remi.thebault@gmail.com>
2022-08-13 09:59:21 +0200

    api attrs: remove JsonBody, DownloadEndpoint

--- packages/common/src/dopamine/api/attrs.d
+++ packages/common/src/dopamine/api/attrs.d
@@ -29,9 +29,6 @@ struct Query
     string name;
 }
 
-/// Decorator for a request Json body
-enum JsonBody;
-
 /// A Decorator to change the name of a Json field
 alias Name = vibe.data.serialization.name;
 
@@ -40,13 +37,6 @@ alias Name = vibe.data.serialization.name;
 struct Response(T)
 {
     private T _ = T.init;
-}
-
-/// Decorator to specify that the request is an endpoint
-/// to download files. Download end-points have several additional
-/// features such as Content-Range, Digest, Content-Disposition, ...
-struct DownloadEndpoint
-{
 }
 
 /// Checks whether `ReqT` is a request type
@@ -99,12 +89,4 @@ template ResponseType(ReqT)
     {
         alias ResponseType = void;
     }
-}
-
-/// Checks whether the request is a download endpoint
-template isDownloadEndpoint(ReqT)
-{
-    static assert(isRequest!ReqT, ReqT.stringof ~ " do not appear to be a valid Request type");
-
-    enum isDownloadEndpoint = hasUDA!(ReqT, DownloadEndpoint);
 }
