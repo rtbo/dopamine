95f11d2c6dfc3fee8fa490d9d1195b01f7b388e1
Remi Thebault <remi.thebault@gmail.com>
2022-08-31 09:25:40 +0200

    options-default test

--- tests_e2e/recipes/options/dopamine.lua
+++ tests_e2e/recipes/options/dopamine.lua
@@ -7,18 +7,24 @@ tools = {'dc'}
 
 options = {
     lib1 = {
-        'boolean',
+        type = 'boolean',
         default = true,
         description = 'Enable lib1 module',
-    }
-    lib2 = true,
+    },
+    lib2 = false,
 }
 
 function build (dirs, config)
     local meson = dop.Meson:new(config.profile)
-
-    meson:setup{build_dir = '.', src_dir = dirs.src, install_dir = dirs.install}
+    meson:setup {
+        build_dir = '.',
+        src_dir = dirs.src,
+        install_dir = dirs.install,
+        defs = {
+            enable_lib1 = config.options.lib1,
+            enable_lib2 = config.options.lib2,
+        },
+    }
     meson:compile()
     meson:install()
 end
-
