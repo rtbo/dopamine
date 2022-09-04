a585593e9fc03a3edde344f81e200030f85c7d55
Remi Thebault <remi.thebault@gmail.com>
2022-09-05 01:09:51 +0200

    code for parsing pkg-config files

---
+++ packages/lib/src/dopamine/pkgconf.d
@@ -0,0 +1,168 @@
+module dopamine.pkgconf;
+
+import std.exception;
+import std.stdio;
+import std.string;
+
+struct PkgConfFile
+{
+    struct Var
+    {
+        string name;
+        string value;
+    }
+
+    Var[] vars;
+
+    string name;
+    string ver;
+    string description;
+    string url;
+    string license;
+    string maintainer;
+    string copyright;
+
+    string[] cflags;
+    string[] cflagsPriv;
+    string[] libs;
+    string[] libsPriv;
+    string[] required;
+    string[] requiredPriv;
+    string[] conflicts;
+    string[] provided;
+
+    static PkgConfFile parse(string filename)
+    {
+        auto f = File(filename, "r");
+
+        PkgConfFile res;
+
+        char[8192] buf;
+        while(true)
+        {
+            char* cline = pkgconf_fgetline(&buf[0], buf.length, f.getFP());
+            if (!cline)
+                break;
+
+            const(char)[] line = cline.fromStringz();
+            line = line.strip();
+            if (line.length)
+                parsePkgConfLine(line.idup, res);
+        }
+
+        return res;
+    }
+}
+
+private:
+
+void parsePkgConfLine(string line, ref PkgConfFile pcf)
+{
+    size_t idx;
+    while(idx < line.length)
+    {
+        const char c = line[idx];
+        if (c == '=')
+        {
+            enforce(idx > 0, "Invalid empty variable name");
+            string ident = line[0 .. idx];
+            string val = line[idx+1 .. $].strip();
+            pcf.vars ~= PkgConfFile.Var(ident, val);
+        }
+        else if (c == ':')
+        {
+            enforce(idx > 0, "Invalid empty keyword");
+            string ident = line[0 .. idx];
+            string val = line[idx+1 .. $].strip();
+            switch (ident)
+            {
+            case "Name":
+                pcf.name = val;
+                break;
+            case "Version":
+                pcf.ver = val;
+                break;
+            case "Description":
+                pcf.description = val;
+                break;
+            case "URL":
+                pcf.url = val;
+                break;
+            case "License":
+                pcf.license = val;
+                break;
+            case "Maintainer":
+                pcf.maintainer = val;
+                break;
+            case "Copyright":
+                pcf.ver = val;
+                break;
+            case "Cflags":
+            case "CFLAGS":
+                pcf.cflags = argvSplit(val);
+                break;
+            case "Cflags.private":
+            case "CFLAGS.private":
+                pcf.cflagsPriv = argvSplit(val);
+                break;
+            case "Libs":
+            case "LIBS":
+                pcf.libs = argvSplit(val);
+                break;
+            case "Libs.private":
+            case "LIBS.private":
+                pcf.libsPriv = argvSplit(val);
+                break;
+            case "Requires":
+                pcf.required = depsSplit(val);
+                break;
+            case "Requires.private":
+                pcf.requiredPriv = depsSplit(val);
+                break;
+            case "Conflicts":
+                pcf.conflicts = depsSplit(val);
+                break;
+            case "Provides":
+                pcf.provided = depsSplit(val);
+                break;
+            default:
+                throw new Exception("Unknown pkg-config keyword: " ~ ident);
+            }
+        }
+        else
+        {
+            idx++;
+        }
+    }
+}
+
+string[] argvSplit(string src)
+{
+    auto csrc = toStringz(src);
+    int argc;
+    char **argv;
+
+    if (pkgconf_argv_split(csrc, &argc, &argv) != 0)
+        throw new Exception("Failed to split args from Pkg-config file: `" ~ src ~ "`");
+
+    string[] res = new string[argc];
+    for(int idx; idx < argc; ++idx)
+    {
+        res[idx] = fromStringz(argv[idx]).idup;
+    }
+    pkgconf_argv_free(argv);
+    return res;
+}
+
+string[] depsSplit(string src)
+{
+    auto deps = src.split(",");
+    foreach (ref dep; deps)
+        dep = dep.strip();
+    return deps;
+}
+
+// a few functions from pkgconf are used.
+extern(C) nothrow char *pkgconf_fgetline(char *line, size_t size, FILE *stream);
+extern(C) nothrow int pkgconf_argv_split(const char *src, int *argc, char ***argv);
+extern(C) nothrow void pkgconf_argv_free(char **argv);
