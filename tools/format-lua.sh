5284f4c22d99292af193588b511a08229a1adf3e
Remi Thebault <remi.thebault@gmail.com>
2022-09-11 10:59:39 +0200

    stylua formatting tool

--- tools/format-lua.sh
+++ tools/format-lua.sh
@@ -3,4 +3,4 @@
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DOPROOT=$( dirname "$THISDIR" )
 
-find "$DOPROOT" -type f -name *.lua -exec lua-format -c "$THISDIR/lua.format" -i {} \;
+find "$DOPROOT" -type f -name *.lua -exec stylua --config-path "$THISDIR/stylua.toml" {} \;
