eec51e46200054f2f3748f4ea2c85588a7c04b97
Remi Thebault <remi.thebault@gmail.com>
2022-09-23 22:27:36 +0200

    add test warning

---
+++ tests_e2e/recipes/warning/dopamine.lua
@@ -0,0 +1,11 @@
+name = 'warning'
+version = '1.0.0'
+description = 'a test library'
+upstream_url = 'https://github.com/rtbo/dopamine'
+license = 'MIT'
+tools = { 'cc' }
+
+warn("A warning from recipe")
+
+function build()
+end
